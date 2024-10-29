c88b819d7cedb909b812779963a6df40179414c6
sholikul <sholikulardian@gmail.com>
2024-10-29 18:53:16 +0700

    menambahkan spasi diantara teks loading, membuat fungsi fetchStudent untung fetching data dan mengirimkan fungsi fetchStudent ke props Form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import { useEffect, useState } from "react";
-import Form from "./components/Form";
-import Table from "./components/Table";
+import { useEffect, useState } from 'react';
+import Form from './components/Form';
+import Table from './components/Table';
 
 const App = () => {
-    // TODO: answer here
-    const [students, setStudents] = useState([]);
-    const [loading, setLoading] = useState(true);
+  // TODO: answer here
+  const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetch('http://localhost:3001/student')
-          .then(response => response.json())
-          .then(data => {
-            setStudents(data);
-            setLoading(false);
-          })
-          .catch(error => console.error("Error fetching data:", error));
-      }, []);
+  const fetchStudent = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/student');
+      const data = await response.json();
+      setStudents(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
-    return (
-        <div>
-          <h1>Student Portal</h1>
-          <Form setStudents={setStudents} />
-            {loading ? <p>Loading...</p> : <Table students={students} setStudents={setStudents} />}
-        </div>
-    );
+  useEffect(() => {
+    fetchStudent();
+  }, []);
+
+  return (
+    <div>
+      <h1>Student Portal</h1>
+      <Form fetchStudent={fetchStudent} />
+      {loading ? <p>Loading ...</p> : <Table students={students} setStudents={setStudents} />}
+    </div>
+  );
 };
 
 export default App;
